788483a2a5e780a828e2fa943dce78ce58510033
Alan Oliveira <79263845+AlanOliveira1998@users.noreply.github.com>
2025-07-14 19:53:36 -0300

    Update 20250707173000-add-barbershop-logo.sql

--- supabase/migrations/20250707173000-add-barbershop-logo.sql
+++ supabase/migrations/20250707173000-add-barbershop-logo.sql
@@ -1,5 +1,5 @@
 -- Adicionar coluna barbershop_logo na tabela profiles
 ALTER TABLE profiles ADD COLUMN barbershop_logo TEXT;
-
+ 
 -- Comentário explicativo
 COMMENT ON COLUMN profiles.barbershop_logo IS 'URL da logo da barbearia armazenada no storage'; 
